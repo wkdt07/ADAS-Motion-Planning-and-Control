2348cfb00e03ef0de0a101c7256fb3fb3406b6bd
Yu_Jung_Hwan <159098195+YUJJHH@users.noreply.github.com>
2025-05-14 14:08:16 +0900

    remove

--- Practice_sample/test.m
+++
@@ -1,4 +0,0 @@
-x = logsout.getElement('Local_Points').Values.Data(:,1);
-y = logsout.getElement('Local_Points').Values.Data(:,2);
-plot(x, y, 'o-'); axis equal; grid on;
-title('Local Waypoints'); xlabel('x [m]'); ylabel('y [m]');
